6e99929c0c8b6bf4bba6460444a17239551f9a6f
Jacob Eaton <me@iamjke.com>
2018-10-18 11:21:24 -0500

    completed phantom entry api

--- src/routes/phantoms/phantom-queries.js
+++ src/routes/phantoms/phantom-queries.js
@@ -1,5 +1,12 @@
 export const getBomd = (itmref, qty) => `
-  SELECT CPNITMREF_0 as partNumber, BOMQTY_0 * ${qty} as qty 
-  FROM BOMD
+  SELECT BOD.CPNITMREF_0 as partNumber, BOD.BOMQTY_0 * ${qty} as qty, ITM.TCLCOD_0 as itemCategory
+  FROM BOMD BOD
+  INNER JOIN ITMMASTER ITM
+  ON BOD.CPNITMREF_0 = ITM.ITMREF_0
+  WHERE BOD.ITMREF_0 = '${itmref}'
+`
+export const getItemCategory = itmref => `
+  SELECT ITMREF_0 as partNumber, TCLCOD_0 as itemCategory
+  FROM ITMMASTER
   WHERE ITMREF_0 = '${itmref}'
 `
--- src/routes/phantoms/phantoms.js
+++ src/routes/phantoms/phantoms.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import bodyParser from "body-parser"
 import couchbase, { N1qlQuery } from "couchbase"
-
+import flattenDeep from "lodash.flattendeep"
+import uuid from "uuid"
 import config from "../../../config.json"
 import SageX3 from "../../db"
-import { getBomd } from "./phantom-queries"
+import { getBomd, getItemCategory } from "./phantom-queries"
 
 // Set up couchbase cluster and bucket //
 const cbConfig = config.couchbase
@@ -17,19 +18,80 @@ const router = Router()
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 
+const asyncBucketUpsert = async (id, doc, _bucket = bucket) => {
+  return new Promise((resolve, reject) => {
+    _bucket.upsert(id, doc, (err, result) => {
+      if (err) reject(err)
+      else {
+        resolve(result)
+      }
+    })
+  })
+}
+
+const asyncBucketGet = async (id, _bucket = bucket) =>
+  new Promise((resolve, reject) => {
+    _bucket.get(id, (err, result) => {
+      if (err.code === 13) resolve(false)
+      else if (err) reject(err)
+      else resolve(result)
+    })
+  })
+
+const newEntryId = async (deviceId, partNumber) => {
+  const UUID = uuid()
+  const entryId = `${deviceId}-${partNumber}-${UUID.substr(UUID.length - 4, 4)}`
+  const result = await asyncBucketGet(entryId)
+  if (result) {
+    newEntryId(deviceId, partNumber)
+  }
+  return entryId
+}
+
 async function getBom(partNumber, qty) {
   const { recordset } = await SageX3.db.query(getBomd(partNumber, qty))
   return recordset
 }
 
+async function isPhantom(partNumber) {
+  const { recordset } = await SageX3.db.query(getItemCategory(partNumber))
+  return recordset[0].itemCategory === "PHANT" ? true : false
+}
+
+async function insertPhantomEntries(bomArray, device, session) {
+  const resultArray = await Promise.all(
+    bomArray.map(async item => {
+      const NOW = new Date().toISOString()
+      const { partNumber, qty } = item
+      const uploadItem = {
+        entryId: await newEntryId(device.deviceId, partNumber),
+        createdAt: NOW,
+        updatedAt: NOW,
+        partNumber,
+        qty: parseFloat(qty),
+        locationID: "8D",
+        type: "entry",
+        void: false,
+        device,
+        session
+      }
+      const results = await asyncBucketUpsert(uploadItem.entryId, uploadItem)
+      return results
+    })
+  )
+  return resultArray
+}
+
 // isBottomLevel if true return bomItem
 async function isBottomLevel(bomItem) {
-  const { partNumber, qty } = bomItem
+  const { partNumber, qty, itemCategory } = bomItem
   const recordset = await getBom(partNumber, qty)
-  if (recordset.length) {
-    const nextLevelRecordSet = await recordset.map(record => {
-      return isBottomLevel(record)
-    })
+  if (recordset.length && itemCategory === "PHANT") {
+    return Promise.all(
+      recordset.map(record => {
+        return isBottomLevel(record)
+      })
+    )
   } else {
     return bomItem
   }
@@ -37,12 +99,42 @@ async function isBottomLevel(bomItem) {
 
 router.post("/entry", async (req, res) => {
   try {
-    const { partNumber, qty } = req.body.phantom
-    const recordset = await getBom(partNumber, qty)
-    const multiLevelBom = await recordset.map(record => {
-      return isBottomLevel(record)
-    })
-    res.status(200).send({ multiLevelBom })
+    const { phantoms, context } = req.body
+    if (!phantoms.length)
+      return res
+        .status(400)
+        .send(`The request body must contain an array of phantoms!`)
+    const results = await Promise.all(
+      phantoms.map(async phantom => {
+        const { partNumber, qty } = phantom
+        const { device, session } = context
+        const recordset = await getBom(partNumber, qty)
+        if (!recordset.length) {
+          return res
+            .status(400)
+            .send({ error: `No BOM found for item ${partNumber}!` })
+        }
+        if ((await isPhantom(partNumber)) === false) {
+          return res.status(400).send({
+            error: `${partNumber} is not a PHANT category in Sage X3!`
+          })
+        }
+        const multiLevelBom = await Promise.all(
+          recordset.map(async record => {
+            const bomItem = await isBottomLevel(record)
+            return bomItem
+          })
+        )
+        const flattenedBom = flattenDeep(multiLevelBom)
+        const resultArray = await insertPhantomEntries(
+          flattenedBom,
+          device,
+          session
+        )
+        return resultArray
+      })
+    )
+    res.status(200).send({ results })
   } catch (error) {
     console.log(error)
   }
